bd12a1747dba78188e1f94f1c77532ab1cf64b0d
patambagraizamai-cpu <patambagraizamai@gmail.com>
2026-08-24 22:24:25 +0800

    Update and rename add or even.c to ionrunit.C

---
+++ ionrunit.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+    int main() {
+        int number = 7;
+
+        if (number % 2 == 0) {
+            printf("%d is Even\n", number);
+        } else {
+            printf("%d is Odd\n", number);
+        }
+
+        return 0;
+    }
