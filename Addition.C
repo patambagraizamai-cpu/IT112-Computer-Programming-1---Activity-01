63bbefdaa169f09900866faad7b2dfcf2ad7c086
patambagraizamai-cpu <patambagraizamai@gmail.com>
2026-08-24 22:25:12 +0800

    Update and rename addition.c to Addition.C

---
+++ Addition.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main() {
+
+        int num1 = 12;
+        int num2 = 8;
+        int sum;
+
+        sum = num1 + num2;
+
+        printf("First number: %d\n", num1);
+        printf("Second number: %d\n", num2);
+        printf("Sum: %d\n", sum);
+
+        return 0;
+    }
